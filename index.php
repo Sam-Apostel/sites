267a97d7556a644144284a1be4eeb80c232a533c
Sam-Apostel <sam@apostel.be>
2017-10-04 11:20:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+
+<div itemscope itemtype='http://schema.org/Person' class='fiverr-seller-widget' style='display: inline-block;'>
+     <a itemprop='url' href=https://www.fiverr.com/samapostel rel="nofollow" target="_blank" style='display: inline-block;'>
+        <div class='fiverr-seller-content' id='fiverr-seller-widget-content-a18fc94f-8216-4b9a-918a-d4c88e683054' itemprop='contentURL' style='display: none;'></div>
+        <div id='fiverr-widget-seller-data' style='display: none;'>
+            <div itemprop='name' >samapostel</div>
+            <div itemscope itemtype='http://schema.org/Organization'><span itemprop='name'>Fiverr</span></div>
+            <div itemprop='jobtitle'>Seller</div>
+            <div itemprop='description'>19y/o web designer w/a passion for vector graphics & animations. Currently @university of Antwerp majoring in computer science. Ps.: The canncelled order on my reviews are due to exams and me not knowing of a vacation mode. Doubt if I can do what your task needs me to do? Just send me a message.</div>
+        </div>
+    </a>
+</div>
+
+<script id='fiverr-seller-widget-script-a18fc94f-8216-4b9a-918a-d4c88e683054' src='https://widgets.fiverr.com/api/v1/seller/samapostel?widget_id=a18fc94f-8216-4b9a-918a-d4c88e683054' data-config='{"category_name":"Graphics & Design"}' async='true' defer='true'></script>
+
+<!-- <?php 
+include('html/nav.html');
+include('html/home.html');
+include('html/aboutMe.html');
+include('html/contact.html');
+include('html/portfolio.html');
+include('html/footer.html');
+?>  --> 
+</body>
+</html>
+
